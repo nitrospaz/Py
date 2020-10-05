af47d231f69cdd2e8b396f8743e9ed89859d6d6f
nitrospaz <36904927+nitrospaz@users.noreply.github.com>
2020-10-05 14:42:35 -0700

    Update Cats_Everywhere.py
    I don't think the cats all needed their own classes for each name. I feel these changes make the code cleaner, more readable and easier to understand. I also added the ability to sing.

--- Cats_Everywhere.py
+++ Cats_Everywhere.py
@@ -1,4 +1,5 @@
-#Original Exercise
+# modified version of class activity
+
 class Pets():
     animals = []
     def __init__(self, animals):
@@ -7,6 +8,10 @@ class Pets():
     def walk(self):
         for animal in self.animals:
             print(animal.walk())
+
+    def sing(self, song):
+        for animal in self.animals:
+            print(animal.sing(song))
 
 class Cat():
     is_lazy = True
@@ -18,19 +23,26 @@ class Cat():
     def walk(self):
         return f'{self.name} is just walking around'
 
-class Simon(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
+    def sing(self, song):
+        return f'{self.name} sings {song}'
 
-class Sally(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
+cat1 = Cat('Simon', 4)
+
+cat2 = Cat('Sally', 12)
 
 #1 Add nother Cat
+cat3 = Cat('Sam', 14)
 
 #2 Create a list of all of the pets (create 3 cat instances from the above)
-my_cats = []
+my_cats = [cat1, cat2, cat3]
 
 #3 Instantiate the Pet class with all your cats use variable my_pets
+my_pets = Pets(my_cats)
 
 #4 Output all of the cats walking using the my_pets instance
+my_pets.walk()
+
+#5 Make the cats sing Star Spangled Banner
+my_pets.sing('Star Spangled Banner')
+
+print(f'{cat1.sing("Voyager Theme Song")} and {cat2.name} hisses as Voyage jumps to warp')
